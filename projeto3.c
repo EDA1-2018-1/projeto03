be796aefbdce09edaadd187b93cb45fb3be75c1f
Max <maxhb.df@gmail.com>
2018-05-15 23:56:27 -0300

    [ADD] more functions, for include a new contat, and little parts of the other functions

--- projeto3.c
+++ projeto3.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//LISTAR AS FUNÇÕES
+
 void inicia(Contato *le);
 //void ordenar (); em ordem alfabética.
 
@@ -16,7 +18,7 @@ typedef struct contato
       char Cel [9];
       char Adress [100];
       int CEP;
-      char Data [3]; //colocar no formato dd/mm/yyyy
+      char Date [3]; //colocar no formato dd/mm/yyyy
 
       struct contato *prox;
       struct contato *ante;
@@ -25,10 +27,29 @@ typedef struct contato
 /*--------------------------------------------------------------------------*/
 
 int main(int argc, char const *argv[])
-    {
+{
+  FILE *arq;                        /*IMPLEMENTAR FUNÇÃO PRA LER ARQUIVO*/
+  char Linha[100];
+  char *result;
+  int i;
 
-      return 0;
-    }
+  arq = fopen("contatos.txt", "r");
+  if (arq == NULL)
+  {
+     printf("Problemas na abertura do arquivo\n");
+     return;
+  }
+  i = 1;
+  while (!feof(arq))
+  {
+      result = fgets(Linha, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
+      if (result)  // Se foi possível ler
+	    printf("Linha %d : %s",i,Linha);
+      i++;
+  }
+  fclose(arq);
+return 0;
+}
 
 /*--------------------------------------------------------------------------*/
 int menu(void)
@@ -45,19 +66,19 @@ int menu(void)
 
 	return opt;
 }
-/*Foi escolhido: libera, inicia, insere, retira, aloca*/
+/*Foi escolhido: limpa, inicia, insere, retira, aloca*/
 //----------------------------------------------------------------------------
-void opcao(Contato *le, int op)
+void opcao(Contato *le, int op) //CONCERTAR OS NOMES DE FUNÇOES
 {
 	Contato *tmp;
 	switch(op)
     {
   		case 0:
-  			libera(Contato);
+  			limpa(Contato);
   			break;
 
   		case 1:
-  			//libera(Contato);
+  			//limpa(Contato);
   			insere(Contato);
   			break;
 
@@ -79,30 +100,89 @@ void opcao(Contato *le, int op)
   	}
 }
 //----------------------------------------------------------------------------
-void inicia(Contato *le)
+void adicionar(/*int parametros*/,Contato *atual) //ACRESCENTAR OS PARAMETROS
 {
-  Contato *le = (Contato *) malloc(sizeof(Contato));
-
-    if(le == NULL)
+    if(*atual == NULL)
       {
-        exit(1);
+        (*atual) = malloc(sizeof(Contato));
+
+        (*atual)->Name = Name;
+        (*atual)->Cel = Cel;
+        (*atual)->Adress = Adress;
+        (*atual)->CEP = CEP;
+        (*atual)->Date = Date;
+
+        (*atual)->prox = *atual;
+        (*atual)->ante = *atual;
       }
-
-    le->Name = Name;
-    le->Cel = Cel;
-    le->Adress = Adress;
-    le->CEP = CEP;
-    le->Data = Data;
-
-	le->prox = NULL;
-  le->ante = NULL;
+    else
+      {
+        Contato *aux, *ultimo;
+        aux = *atual;
+        while(aux->prox != *atual)
+            aux = aux->prox;
+        aux->prox = malloc(sizeof(Contato));
+        ultimo = aux->prox;
+        ultimo->ante = aux;
+        ultimo->prox = *atual;
+        ultimo->info = parametro;
+        (*atual)->ante = ultimo;
+      }
 }
 //----------------------------------------------------------------------------
-void imprime (Contato *le)
+void ordena(Lista *le){ //insertion sort
+    Contato *cur, *ptr, *tmp, *aux, *aux_ant, *aux_prox;
+//  if(listaVazia(le))
+//      return;
+
+    cur = *le;
+    cur = cur->prox;
+
+    // Tratando para o caso da lista com 2 elementos
+    if( cur->prox == *le){
+        if( (*le)->info > cur->info)
+            (*le) = (*le)->prox;
+        return;
+    }
+
+    while(cur!=*le){
+        ptr=cur;
+        tmp=cur->ant;
+        cur=cur->prox;
+
+        while (tmp->prox != *le && tmp->info > ptr->info){
+            aux_ant = tmp->ant;
+            aux_prox = ptr->prox;
+
+            aux_ant->prox = ptr;
+            ptr->prox = tmp;
+            tmp->prox = aux_prox;
+            aux_prox->ant = tmp;
+            tmp->ant = ptr;
+            ptr->ant = aux_ant;
+
+            tmp = ptr;
+            ptr = tmp->prox;
+
+            if(ptr==*le)
+                *le = tmp;
+
+            tmp=tmp->ant;
+            ptr = ptr->ant;
+        }
+    }
+}
+//----------------------------------------------------------------------------
+void imprime (Contato *le) //tem que refatorar
   {
-     if (le != NULL) {
-        printf ("%d\n", le->Contato);
-        imprime (le->prox);
+    Contato *aux;
+    aux = *atual;
+    printf("Imprimindo lista:\n");
+    do{
+        printf("%d ", aux->info);
+        aux = aux->prox;
+    }while(aux != *atual);
+    printf("\n");
      }
   }
 //-----------------------------------------------------------------------------
@@ -115,23 +195,41 @@ Contato *busca_contato (int x, Contato *le) //Recebe x
       return le;
   return busca_contato (x, le->prox);
   }
-  //---------------------------------------------------------------------------
-  Contato *retiraFim(Contato *LISTA)
+//-----------------------------------------------------------------------------
+Contato *retira(Contato *le)//FAZER ESSA FUNÇÃO
 {
-	if(LISTA->prox == NULL){
-		printf("Lista ja vazia\n\n");
-		return NULL;
-	}else{
-		Contato *ultimo = LISTA->prox,
-			 *penultimo = LISTA;
+	if(le->prox == NULL)
+    {
+  		printf("Lista ja vazia\n\n");
+  		return NULL;
+  	}
+  else
+    {
+  		Contato *ultimo = le->prox,
+  			 *penultimo = le;
 
-		while(ultimo->prox != NULL){
-			penultimo = ultimo;
-			ultimo = ultimo->prox;
-		}
+  		while(ultimo->prox != NULL){
+  			penultimo = ultimo;
+  			ultimo = ultimo->prox;
+  	}
 
 		penultimo->prox = NULL;
 		tam--;
 		return ultimo;
+	}
+}
+//-----------------------------------------------------------------------------
+void limpa(node *le)//FAZER ESSA FUNÇÃO
+{
+	if(!vazia(le)){
+		node *proxNode,
+			  *atual;
+
+		atual = le->prox;
+		while(atual != NULL){
+			proxNode = atual->prox;
+			free(atual);
+			atual = proxNode;
+		}
 	}
 }
